a52e028bef043ca5c9c839e69bf77e7cc62d8c74
Finn_Dane <mailaanfinn@gmail.com>
2021-02-14 14:13:58 +0100

    Added scripting capability, hopefully...

--- README.md
+++ README.md
@@ -1,6 +1,64 @@
+
 # BlueprintTool
 A CLI blueprintTool
 
-The reason I made this was because I wanted a simple and fast solution for logic projects and didn't like having to use a GUI for that.
+I made this was because I wanted a simple and fast solution for logic projects and didn't like having to use a GUI for that.
 
-Currently only has a connection feature, because that was what I made it for, I might add more features.
+Currently only has a connection feature with a way to make "scripts" in JSON, I will probably add more features later.
+
+Disclaimer: this is absolute shitcode
+
+## Basic usage
+```
+blueprintTool.py [-h] (-b BLUEPRINT | -i INPUT) [-c SRC DST]
+```
+You can use Hex for the colors or the format: "\<baseColor>-\<row>" where \<baseColor> is one of the following:
+
+`gray, yellow, lime, green, cyan, blue, violet, magenta, red or orange`
+
+capitalization does not matter.
+
+and \<row> are their vertical variants numbered 1 through 4 respectively 
+## Scripting
+Use the -i/-\-input flags to specify the json file.
+
+Use this format for the JSON file:
+```json
+{
+	"<blueprintName>":
+	[
+		[
+			"<function>",
+			"<argument1>",
+			"<argument2>",
+			"<etc...>"
+		]
+	]
+}
+```
+Example script:
+```json
+{
+	"blueprint1":
+	[
+		[
+			"connect",
+			"eeeeee",
+			"222222"
+		],
+		[
+			"connect",
+			"gray-1",
+			"orange-1"
+		]
+	],
+	"blueprint2":
+	[
+		[
+			"connect",
+			"ffffff",
+			"eeeeee"
+		]
+	]
+}
+```
--- blueprintTool.py
+++ blueprintTool.py
@@ -8,14 +8,14 @@ from colors import colorLookup
 #Arguments
 argParser = argparse.ArgumentParser()
 
-argParser.add_argument("blueprint", help = "the name of the blueprint you are trying to edit")
+group = argParser.add_mutually_exclusive_group(required=True)
+group.add_argument("-b", "--blueprint", help = "the name of the blueprint you are trying to edit")
+group.add_argument("-i", "--input", help = "the path of the script you want to use")
 argParser.add_argument("-c", "--connect", nargs = 2, metavar = ("SRC", "DST"), help = "connect logic gates of color SRC to logic gates of color DST (use this format: <baseColor>-<1-4> or hex directly for color)")
+
 args = argParser.parse_args()
 
-def parseColor(input):
-	if(input in colorLookup):
-		return colorLookup[input]
-	return input
+
 
 #Simple helper class to store paths
 class paths:
@@ -24,54 +24,99 @@ class paths:
    userPath = basePath + re.search("User_[0-9]{17}", "".join(os.listdir(basePath))).group()
    blueprints = userPath + "\\Blueprints"
 
-#Main
-found = False
-multipleFound = False
-for BpDirItterator in os.listdir(paths.blueprints):
-	try:
-		with open("%s\\%s\\description.json" %(paths.blueprints, BpDirItterator), "r") as descriptionFile:
-			name = json.load(descriptionFile)["name"]
-			if(name == args.blueprint):
-				multipleFound = found
-				found = True
-				bpDir = BpDirItterator
-	except:
-		pass
+def error(msg):
+	print(msg)
+	exit()
 
-if(found):
-	if(multipleFound):
-		print("Multiple blueprints with the name \"%s\" have been found, please save the blueprint with a unique name." %(args.blueprint))
-	else:
-		destinations = []
+def parseColor(input):
+	if(input.lower() in colorLookup):
+		return colorLookup[input.lower()]
+	return input
+
+def connect(src, dst, JSON):
+	destinations = []
+	#loop over all parts to detect if they are a destination
+	for body in JSON["bodies"]:
+		for child in body["childs"]:
+			if(child["color"] == parseColor(dst).lower()):	#if current part is destination color
+				destinations.append({"id": child["controller"]["id"]})
+
+	#loop over all parts again to write the destinations to the sources
+	for body in JSON["bodies"]:
+		for child in body["childs"]:
+			if(child["color"] == parseColor(src).lower()):	#if current part is source color
+				for destination in destinations:
+
+					if child["controller"]["controllers"] == None:	#check if the part already has connections and if not just write the whole array directly 
+						child["controller"]["controllers"] = destinations
+					else:	#if it already has connections check if the connections alreay exist and if not append them to the array
+						if destination not in child["controller"]["controllers"]:
+							child["controller"]["controllers"].append(destination)
+
+def searchBlueprint(bpName):
+	found = False
+	multipleFound = False
+	for BpDirItterator in os.listdir(paths.blueprints):
 		try:
-			with open("%s\\%s\\blueprint.json" %(paths.blueprints, bpDir), "r+") as blueprintFile:
-				blueprintJson = json.load(blueprintFile)
-				if(args.connect):	#connect mode
-					#loop over all parts to detect if they are a destination
-					for body in blueprintJson["bodies"]:
-						for child in body["childs"]:
-							if(child["color"] == parseColor(args.connect[1]).lower()):	#if current part is destination color
-								destinations.append({"id": child["controller"]["id"]})
+			with open(f"{paths.blueprints}\\{BpDirItterator}\\description.json", "r") as descriptionFile:
+				name = json.load(descriptionFile)["name"]
+				if(name == bpName):
+					multipleFound = found
+					found = True
+					bpDir = BpDirItterator
+		except:
+			pass
+	if(multipleFound):
+		error(f"Multiple blueprints with the name \"{bpName}\" have been found, please save the blueprint with a unique name.")
+	if(not found):
+		error(f"No blueprint with the name of \"{bpName}\" was found.")
+	return bpDir
 
-					#loop over all parts again to write the destinations to the sources
-					for body in blueprintJson["bodies"]:
-						for child in body["childs"]:
-							if(child["color"] == parseColor(args.connect[0]).lower()):	#if current part is source color
-								for destination in destinations:
-									if child["controller"]["controllers"] == None:	#check if the part already has connections and if not just write the whole array directly 
-										child["controller"]["controllers"] = destinations
-									else:	#if it already has connections check if the connections alreay exist and if not append them to the array
-										if destination not in child["controller"]["controllers"]:
-											child["controller"]["controllers"].append(destination)
+
+
+#Main
+if(args.input):	#if file/script input
+	try:
+		with open(f"{args.input}", "r") as scriptFile:
+			scriptJson = json.load(scriptFile)
+			
+			for blueprintName in scriptJson:
+				blueprintDir = searchBlueprint(blueprintName)
 				
-				#overwrite file
-				blueprintFile.seek(0)
-				blueprintFile.write(json.dumps(blueprintJson))
-				blueprintFile.truncate()
+				try:
+					with open(f"{paths.blueprints}\\{blueprintDir}\\blueprint.json", "r+") as blueprintFile:	
+						blueprintJson = json.load(blueprintFile)
+					
+						for command in scriptJson[blueprintName]:
+						
+							if(command[0] == "connect"):
+								if(len(command) != 3): error("the command 'connect' requires 2 arguments")
+								connect(command[1], command[2], blueprintJson)
+							else: error(f"command: \"{command[0]}\" is not recognised")
+					
+						#overwrite file
+						blueprintFile.seek(0)
+						blueprintFile.write(json.dumps(blueprintJson))
+						blueprintFile.truncate()
+				
+				except Exception as e:
+					error(f"error while reading blueprint file\n{e}")
 
-		except Exception as e:
-			print("error while reading blueprint file")
-			print(e)
+	except Exception as e:
+		error(f"error while reading script\n{e}")
+
 else:
-	print("No blueprint with the name of \"%s\" was found." %(args.blueprint))
+	blueprintDir = searchBlueprint(args.blueprint)
+	try:
+		with open(f"{paths.blueprints}\\{blueprintDir}\\blueprint.json", "r+") as blueprintFile:
+			blueprintJson = json.load(blueprintFile)
 
+			if(args.connect):
+				connect(args.connect[0], args.connect[1], blueprintJson)
+
+			#overwrite file
+			blueprintFile.seek(0)
+			blueprintFile.write(json.dumps(blueprintJson))
+			blueprintFile.truncate()
+	except Exception as e:
+		error(f"error while reading blueprint file\n{e}")
